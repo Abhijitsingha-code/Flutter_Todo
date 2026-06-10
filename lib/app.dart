01ec905a79634dd2b283801702f63317a4ea10b9
Abhijit Singha <abhijit03.pro@gmail.com>
2026-06-10 23:43:09 +0530

    chore: rename project to My Todo and update branding assets across all platforms

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'TaskFlow',
+      title: 'My Todo',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       routerConfig: appRouter,
